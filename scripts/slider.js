18e1b1e815a5872fbcd6dd5c818afb74296144a6
awqsomee <awqsome@bk.ru>
2023-02-21 15:18:36 +0300

    slider

---
+++ scripts/slider.js
@@ -0,0 +1,60 @@
+let slides = []
+let dots = []
+let viewportWidth = window.innerWidth
+let slideIndex = 0
+
+;(function () {
+  slides = document.getElementsByClassName('item')
+  window.onload = init
+
+  function init() {
+    slides[0].style.transform = `translateX(0px)`
+    if (slides.length <= 1) {
+      return
+    }
+    document.querySelectorAll('arrow').forEach((arrow) => (arrow.style.opacity = '1'))
+    let dotSpace = document.querySelector('dots')
+    for (let i = 0; i < slides.length; i++) {
+      let dot = document.createElement('div')
+      dot.className = 'dot'
+      dot.setAttribute('onclick', `toSlide(${i})`)
+      dotSpace.appendChild(dot)
+    }
+    dots = document.getElementsByClassName('dot')
+    dots[0].classList.add('active')
+    dotSpace.style.opacity = '1'
+  }
+})()
+
+const nextSlide = () => {
+  if (slideIndex >= slides.length - 1) return
+  slides[slideIndex].style.transform = `translateX(-${viewportWidth}px)`
+  dots[slideIndex].classList.remove('active')
+  slideIndex++
+  slides[slideIndex].style.transform = `translateX(0px)`
+  dots[slideIndex].classList.add('active')
+}
+
+const previousSlide = () => {
+  if (slideIndex <= 0) return
+  slides[slideIndex].style.transform = `translateX(${viewportWidth}px)`
+  dots[slideIndex].classList.remove('active')
+  slideIndex--
+  slides[slideIndex].style.transform = `translateX(0px)`
+  dots[slideIndex].classList.add('active')
+}
+
+const toSlide = (slideNum) => {
+  dots[slideIndex].classList.remove('active')
+  if (slideIndex < slideNum)
+    for (let i = 0; i < slideNum; i++) slides[i].style.transform = `translateX(-${viewportWidth}px)`
+  slides[slideNum].style.transform = `translateX(0px)`
+
+  if (slideIndex > slideNum) {
+    for (let i = slides.length - 1; i > slideNum; i--)
+      slides[i].style.transform = `translateX(${viewportWidth}px)`
+    slides[slideNum].style.transform = `translateX(0px)`
+  }
+  slideIndex = slideNum
+  dots[slideIndex].classList.add('active')
+}
