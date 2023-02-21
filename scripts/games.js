b6fdebed14a758a67fc46c7c74ad50e71c391a9a
awqsomee <awqsome@bk.ru>
2023-02-21 05:52:48 +0300

    major update

--- scripts/games.js
+++ scripts/games.js
@@ -1,3 +1,19 @@
 function bord3() {
-  document.getElementById('bg').classList.toggle('borderlands_3')
+  document.getElementById('bg').animate(
+    {
+      backgroundColor: 'red', // offset: 0, 0.5, 1
+    },
+    { duration: 2000, iterations: 1 }
+    // [
+    //   {},
+    //   {
+    //     // to
+    //     backgroundImage: url('https://unsplash.it/600/600'),
+    //   },
+    // ],
+    // {
+    //   duration: 2000,
+    //   iterations: 1,
+    // }
+  )
 }
